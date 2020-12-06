abd5f42b0196eb4e267b9e2fa40c4620fb089985
one1pk <75516157+one1pk@users.noreply.github.com>
2020-12-06 15:05:50 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 alert('Number of Covid Cases: 1')
 chrome.runtime.onMessage.addListener(function (request) {
     const re = new RegExp('bear', 'gi')
-    const matches = doucment.doucmentElement.innerHTML.match(re)
-    document.doucmentElement.innerHTML.match(re)
+    const matches = document.documentElement.innerHTML.match(re)
+    document.documentElement.innerHTML.match(re)
 })
