5283a7cbf796ac43d3689ba6849bf23794c9e380
Ray Ho <msndotnet@hotmail.com>
2020-12-06 16:27:54 -0800

    Changed id to class

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
-alert('Number of Covid Cases: 1')
-chrome.runtime.onMessage.addListener(function (request) {
-    const re = new RegExp('bear', 'gi')
-    const matches = document.documentElement.innerHTML.match(re)
-    document.documentElement.innerHTML.match(re)
-})
+const container = document.querySelector('.cXedhc');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'COVID-19 Cases Reported Here.';
+
+container.appendChild(content);
