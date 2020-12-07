10ecd2310f349752e6177817f2066e9286fc653e
one1pk <75516157+one1pk@users.noreply.github.com>
2020-12-06 16:55:06 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,23 @@
-const container = document.querySelector('.cXedhc');
+// General protocol
+const container1 = document.querySelector('.AEprdc vk_c');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = '⚠️COVID-19 Cases Reported Here.';
+const content1 = document.createElement('div');
+content1.classList.add('content1');
+content1.textContent = 'Remember to go out with only immediate household members.';
 
-container.appendChild(content);
+content1.appendChild(container1);
+
+const content1A = document.createElement('div');
+content1A.classList.add('content1A');
+content1A.textContent = 'COVID-19 alert';
+
+content1A.appendChild(content1);
+
+// Location tag
+const container2 = document.querySelector('.cXedhc');
+
+const content2 = document.createElement('div');
+content2.classList.add('content2');
+content2.textContent = '❗ COVID-19 Cases Reported Here.';
+
+container2.appendChild(content2);
